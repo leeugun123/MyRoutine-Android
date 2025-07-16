d1145043659436781e230a3fa6d098d01531b3ed
leeugun123 <leeugun123@naver.com>
2025-07-16 21:18:58 +0900

    init commit

---
+++ app/src/main/java/com/example/myroutine/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.example.myroutine
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.myroutine.ui.theme.MyRoutineTheme
+
+class MainActivity : ComponentActivity() {
+
+    private val WEB_URL = "https://687793d89f558cb8375cbee7--calm-profiterole-ad8f27.netlify.app/#/"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyRoutineTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPaddingValue ->
+                    WebViewScreen(WEB_URL,innerPaddingValue)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewScreen(urlToRender: String, innerPaddingValue : PaddingValues) {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                webViewClient = WebViewClient()
+                settings.javaScriptEnabled = true
+                loadUrl(urlToRender)
+            }
+        },
+        modifier = Modifier.fillMaxSize().padding(innerPaddingValue)
+    )
+}
